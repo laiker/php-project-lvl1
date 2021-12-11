75932292f713e8e72e0e812e453f89716cffd4bd
Ruslan Demin <laikernest@gmail.com>
2021-12-11 13:49:01 +0700

    tasks 2,3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '01d93f326849bdadb166a2c5b7710824060d8f00',
+        'name' => 'hexlet/code',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hexlet/code' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '01d93f326849bdadb166a2c5b7710824060d8f00',
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.11.13',
+            'version' => '0.11.13.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'reference' => 'a2866855ac1abc53005c102e901553ad5772dc04',
+            'dev_requirement' => false,
+        ),
+    ),
+);
